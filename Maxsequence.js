b551279f5bbaf55285bdb578f92be08f7121d589
wrontc13 <wrontc13@gmail.com>
2016-05-11 21:25:27 -0400

    Add files via upload

---
+++ Maxsequence.js
@@ -0,0 +1,30 @@
+﻿function findMaxSequence(array) {
+    var previousVal;
+    var longestSequence = [];
+    var currentSequence = [];
+
+    array.forEach(function (val, i, arr) {
+        if (i === 0 || previousVal < val) {
+            currentSequence.push(val);
+        } else {
+           
+            currentSequence = [val];
+        }
+
+        previousVal = val;
+
+        
+        if (longestSequence.length < currentSequence.length) {
+            longestSequence = currentSequence;
+        }
+    });
+
+    return "[" + longestSequence.join(", ") + "]";
+}
+
+var div = document.getElementById('data')
+var div2 = document.getElementById('data2')
+var div3 = document.getElementById('data3')
+div.textContent = findMaxSequence([3, 2, 3, 4, 2, 2, 4]);
+div2.textContent = findMaxSequence([3, 5, 4, 6, 1, 2, 3, 6, 10, 32]);
+div3.textContent = findMaxSequence([3, 2, 1])
